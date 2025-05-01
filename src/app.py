8817bac58a0c0818711c6f8d9181de7cf6e6637a
Igor Pivnev <iq.pivnev@gmail.com>
2025-05-01 20:12:40 +0300

    feat: add simple Streamlit UI

---
+++ src/app.py
@@ -0,0 +1,47 @@
+import requests
+import streamlit as st
+from utils import response_generator, startup_page_ui
+
+
+def main():
+    session_id = startup_page_ui()
+
+    if 'messages' not in st.session_state:
+        st.session_state.messages = [
+            {'role': 'assistant', 'content': 'How can I help you?'}
+        ]
+
+    if prompt := st.chat_input('Enter the text of your appeal'):
+        st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+    for message in st.session_state.messages:
+        with st.chat_message(message['role']):
+            st.write(message['content'])
+
+    if st.session_state.messages[-1]['role'] != 'assistant':
+        with st.chat_message('assistant'):
+            with st.spinner('Gathering information'):
+                history_message = {
+                    'session_id': session_id,
+                    'history': [{'role': 'user', 'content': prompt}],
+                }
+
+                try:
+                    response = requests.post(
+                        'http://fastapi:80/api/v1/get_answer/',
+                        json=history_message,
+                    )
+                    response.raise_for_status()
+                    answer = response.json()
+
+                    response = st.write_stream(response_generator(answer['answer']))
+                    st.session_state.messages.append(
+                        {'role': 'assistant', 'content': response}
+                    )
+
+                except requests.exceptions.RequestException as e:
+                    st.error(f'An error occurred: {e}')
+
+
+if __name__ == '__main__':
+    main()
