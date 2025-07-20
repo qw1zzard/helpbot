9aec8bb7a83e3ec50effa56f6b28516c425797ca
Igor Pivnev <iq.pivnev@gmail.com>
2025-07-20 18:03:24 +0300

    fix: Telegram bot cannot start with Streamlit

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-uvicorn src.main:app --host localhost --port 80 &
+uvicorn src.main:app --host 0.0.0.0 --port 80 &
 
 until curl -s http://localhost:80/ > /dev/null; do
     sleep 1
 done
 
-streamlit run src/ui/app.py --server.port 8501
+streamlit run src/ui/app.py --server.port 8501 &
 python src/telegram/bot.py
